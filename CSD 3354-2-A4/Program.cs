aea078314b31736f60e3cbf64486fad9c9d64343
visualstudio <visualstudio@122-PC32>
2019-03-27 09:54:56 -0400

    01

--- CSD 3354-2-A4/Program.cs
+++ CSD 3354-2-A4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSD_3354_2_A4
@@ -10,6 +11,15 @@ namespace CSD_3354_2_A4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Downloading File");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download Complete");
+        }
         }
     }
-}
+
